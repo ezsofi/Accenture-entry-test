9f3ea248442fbee1918a44cdd6dd503cbcc56c03
ezsofi <ezsofi@users.noreply.github.com>
2018-03-22 09:33:54 +0100

    Add files via upload

---
+++ Accenture_Excercise2.py
@@ -0,0 +1,17 @@
+text="apple alarm"
+
+def encode_text(text):
+	text.lower()
+	first_letter = text[0]
+	num_fl = text.count(first_letter)
+	encoded_text = ""
+	for char in text:
+		if char.isalpha():
+			num_char = ord(char) + num_fl 
+			if num_char > ord('z'):
+				num_char = num_char-26
+			encoded_char = chr(num_char)
+		encoded_text += encoded_char
+	return encoded_text
+
+print(encode_text(text))
